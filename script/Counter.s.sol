a911e0189b945485073dd73b4d16f49735a94fd4
PirateCode <piratecodeweb3@gmail.com>
2024-10-25 18:55:15 +1100

    init

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,19 +1,34 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: CC0-1.0
+pragma solidity ^0.8.27;
 
-import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {console2} from "forge-std/console2.sol";
+import {Script} from "forge-std/Script.sol";
+import {Dai} from "../src/Dai.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+interface ImmutableCreate2Factory {
+    function safeCreate2(bytes32 salt, bytes calldata initCode) external payable returns (address deploymentAddress);
+    function findCreate2Address(bytes32 salt, bytes calldata initCode)
+        external
+        view
+        returns (address deploymentAddress);
+    function findCreate2AddressViaHash(bytes32 salt, bytes32 initCodeHash)
+        external
+        view
+        returns (address deploymentAddress);
+}
 
-    function setUp() public {}
+contract Deploy is Script {
+    ImmutableCreate2Factory immutable factory = ImmutableCreate2Factory(0x0000000000FFe8B47B3e2130213B802212439497);
+    bytes initCode = type(Dai).creationCode;
+    bytes32 salt = 0x0000000000000000000000000000000000000000f8c87f7f5c3eb00006a9f5d8;
 
-    function run() public {
+    function run() external {
         vm.startBroadcast();
 
-        counter = new Counter();
-
+        address daiAddress = factory.safeCreate2(salt, initCode);
+        Dai dai = Dai(daiAddress);
+        console2.log(address(dai));
+	
         vm.stopBroadcast();
     }
 }
---
+++ test/GetInitCodeHashTest.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: CC0-1.0
+pragma solidity ^0.8.27;
+
+import "forge-std/console.sol";
+import "forge-std/Test.sol";
+/* import "ds-test/test.sol"; */
+import "../src/Dai.sol";
+
+contract GetInitCodeHashTest is Test {
+    function testGetDaiInitCodeHash() public pure {
+        bytes memory bytecode = type(Dai).creationCode;
+        console.logBytes32(keccak256(abi.encodePacked(bytecode)));
+    }
+}
